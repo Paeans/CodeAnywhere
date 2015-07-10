d6f7130b64c52fdd242ff655cf162811458d89e6
paeans <paeans@gmail.com>
2015-07-10 13:23:32 -0400

    Reverse test code

--- 6.00.1X/Chapter00/test.py
+++ 6.00.1X/Chapter00/test.py
@@ -1,5 +1,3 @@
-import sys
-versionTag = sys.version_info[0]
 
 def isPal(x):
     assert type(x) == list
@@ -21,3 +19,56 @@ def silly(n):
         print('No')
         
 silly(int(raw_input("Input a number:")))
+
+def FancyDivide(list_of_numbers, index):
+    try:
+        try:
+            raise Exception("0")
+        finally:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+    except Exception, e:
+        print e
+        
+FancyDivide([0, 2, 4], 1)
+
+a = [1, 2, 3, 4]
+print (a)
+print [tmp for tmp in a]
+try:
+    assert(0 == 1), 'NO'
+except AssertionError, e:
+    print 'in except part', e 
+    
+SCRABBLE_LETTER_VALUES = {
+    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+}
+
+print SCRABBLE_LETTER_VALUES['a']
+
+def getWordScore(word, n):
+    """
+    Returns the score for a word. Assumes the word is a valid word.
+
+    The score for a word is the sum of the points for letters in the
+    word, multiplied by the length of the word, PLUS 50 points if all n
+    letters are used on the first turn.
+
+    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
+    worth 3, D is worth 2, E is worth 1, and so on (see SCRABBLE_LETTER_VALUES)
+
+    word: string (lowercase letters)
+    n: integer (HAND_SIZE; i.e., hand size required for additional points)
+    returns: int >= 0
+    """
+    result = 0
+    for char in word:
+        result += SCRABBLE_LETTER_VALUES[char]
+    result *= len(word)
+    if len(word) == n:
+        result += 50
+    return result
+
+tmp = ['a', 'b']
+if not len(tmp) == 0: print 'YES'
