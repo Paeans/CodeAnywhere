a839fd211e4449e29b8746c7f6dc2b08e024728c
paeans <paeans@gmail.com>
2015-08-07 14:29:38 -0400

    Update

---
+++ 6.00.1X/Quiz_Final/main.py
@@ -0,0 +1,325 @@
+def uniqueValues(aDict):
+    '''
+    aDict: a dictionary
+    returns: a sorted list of keys that map to unique aDict values, empty list if none
+    '''
+    # Your code here
+    tmp = {}
+    result = []
+    for value in aDict.values():
+        if(value in tmp.keys()): tmp[value] += 1
+        else: tmp[value] = 1
+    for key in aDict.keys():
+        if(tmp[aDict[key]] == 1): result.append(key)
+    return sorted(result)
+
+class courseInfo(object):
+
+    def __init__(self, courseName):
+        self.courseName = courseName
+        self.psetsDone = []
+        self.grade = "No Grade"
+        
+    def setPset(self, pset, score):
+        self.psetsDone.append((pset, score))
+        
+    def getPset(self, pset):
+        for (p, score) in self.psetsDone:
+            if p == pset:
+                return score
+
+    def setGrade(self, grade):
+        if self.grade == "No Grade":
+            self.grade = grade
+
+    def getGrade(self):
+        return self.grade
+
+
+
+class edx(object):
+    def __init__(self, courses):
+        self.myCourses = []
+        for course in courses:
+            self.myCourses.append(courseInfo(course))
+
+    def setGrade(self, grade, course="6.01x"):
+        """
+        grade: integer greater than or equal to 0 and less than or equal to 100
+        course: string 
+
+        This method sets the grade in the courseInfo object named by `course`.   
+
+        If `course` was not part of the initialization, then no grade is set, and no
+        error is thrown.
+
+        The method does not return a value.
+        """
+        #   fill in code to set the grade
+        for courseinfo in self.myCourses:
+            if courseinfo.courseName == course: courseinfo.setGrade(grade)
+
+    def getGrade(self, course="6.02x"):
+        """
+        course: string 
+
+        This method gets the grade in the the courseInfo object named by `course`.
+
+        returns: the integer grade for `course`.  
+        If `course` was not part of the initialization, returns -1.
+        """
+        #   fill in code to get the grade
+        for courseinfo in self.myCourses:
+            if courseinfo.courseName == course: return courseinfo.getGrade()
+        return -1
+
+    def setPset(self, pset, score, course="6.00x"):
+        """
+        pset: a string or a number
+        score: an integer between 0 and 100
+        course: string
+
+        The `score` of the specified `pset` is set for the
+        given `course` using the courseInfo object.
+
+        If `course` is not part of the initialization, then no pset score is set,
+        and no error is thrown.
+        """
+        #   fill in code to set the pset
+        for courseinfo in self.myCourses:
+            if courseinfo.courseName == course: courseinfo.setPset(pset, score)
+
+    def getPset(self, pset, course="6.00x"):
+        """
+        pset: a string or a number
+        course: string        
+
+        returns: The score of the specified `pset` of the given
+        `course` using the courseInfo object.
+        If `course` was not part of the initialization, returns -1.
+        """
+        #   fill in code to get the pset
+        for courseinfo in self.myCourses:
+            if courseinfo.courseName == course: return courseinfo.getPset(pset)
+        return -1
+
+edX = edx( ["6.00x","6.01x","6.02x"] )
+edX.setPset(1,100)
+edX.setPset(2,100,"6.00x")
+edX.setPset(2,90,"6.00x")
+
+edX.setGrade(100)
+
+for c in ["6.00x","6.01x","6.02x"]:
+    edX.setGrade(90,c)
+
+    
+class Person(object):
+    def __init__(self, name):
+        #create a person with name name
+        self.name = name
+        try:
+            firstBlank = name.rindex(' ')
+            self.lastName = name[firstBlank+1:]
+        except:
+            self.lastName = name
+        self.age = None
+    def getLastName(self):
+        #return self's last name
+        return self.lastName
+    def setAge(self, age):
+        #assumes age is an int greater than 0
+        #sets self's age to age (in years)
+        self.age = age
+    def getAge(self):
+        #assumes that self's age has been set
+        #returns self's current age in years
+        if self.age == None:
+            raise ValueError
+        return self.age
+    def __lt__(self, other):
+        #return True if self's name is lexicographically less
+        #than other's name, and False otherwise
+        if self.lastName == other.lastName:
+            return self.name < other.name
+        return self.lastName < other.lastName
+    def __str__(self):
+        #return self's name
+        return self.name
+        
+class USResident(Person):
+    """ 
+    A Person who resides in the US.
+    """
+    def __init__(self, name, status):
+        """ 
+        Initializes a Person object. A USResident object inherits 
+        from Person and has one additional attribute:
+        status: a string, one of "citizen", "legal_resident", "illegal_resident"
+        Raises a ValueError if status is not one of those 3 strings
+        """
+        # Write your code here
+        if status not in [ "citizen", "legal_resident", "illegal_resident" ]:
+            raise ValueError
+        Person.__init__(self, name)
+        self.status = status
+        
+    def getStatus(self):
+        """
+        Returns the status
+        """
+        # Write your code here
+        return self.status
+
+
+class Member(object):
+    def __init__(self, founder):
+        """ 
+        founder: string
+        Initializes a member. 
+        Name is the string of name of this node,
+        parent is None, and no children
+        """        
+        self.name = founder
+        self.parent = None         
+        self.children = []    
+
+    def __str__(self):
+        return self.name    
+
+    def add_parent(self, mother):
+        """
+        mother: Member
+        Sets the parent of this node to the `mother` Member node
+        """
+        self.parent = mother   
+
+    def get_parent(self):
+        """
+        Returns the parent Member node of this Member
+        """
+        return self.parent 
+
+    def is_parent(self, mother):
+        """
+        mother: Member
+        Returns: Boolean, whether or not `mother` is the 
+        parent of this Member
+        """
+        return self.parent == mother  
+
+    def add_child(self, child):
+        """
+        child: Member
+        Adds another child Member node to this Member
+        """
+        self.children.append(child)   
+
+    def is_child(self, child):
+        """
+        child: Member
+        Returns: Boolean, whether or not `child` is a
+        child of this Member
+        """
+        return child in self.children 
+
+
+class Family(object):
+    def __init__(self, founder):
+        """ 
+        Initialize with string of name of oldest ancestor
+
+        Keyword arguments:
+        founder -- string of name of oldest ancestor
+        """
+
+        self.names_to_nodes = {}
+        self.root = Member(founder)    
+        self.names_to_nodes[founder] = self.root   
+
+    def set_children(self, mother, list_of_children):
+        """
+        Set all children of the mother. 
+
+        Keyword arguments: 
+        mother -- mother's name as a string
+        list_of_children -- children names as strings
+        """
+        # convert name to Member node (should check for validity)
+        mom_node = self.names_to_nodes[mother]   
+        # add each child
+        for c in list_of_children:           
+            # create Member node for a child   
+            c_member = Member(c)               
+            # remember its name to node mapping
+            self.names_to_nodes[c] = c_member    
+            # set child's parent
+            c_member.add_parent(mom_node)        
+            # set the parent's child
+            mom_node.add_child(c_member)         
+    
+    def is_parent(self, mother, kid):
+        """
+        Returns True or False whether mother is parent of kid. 
+
+        Keyword arguments: 
+        mother -- string of mother's name
+        kid -- string of kid's name
+        """
+        mom_node = self.names_to_nodes[mother]
+        child_node = self.names_to_nodes[kid]
+        return child_node.is_parent(mom_node)   
+
+    def is_child(self, kid, mother):
+        """
+        Returns True or False whether kid is child of mother. 
+
+        Keyword arguments: 
+        kid -- string of kid's name
+        mother -- string of mother's name
+        """        
+        mom_node = self.names_to_nodes[mother]   
+        child_node = self.names_to_nodes[kid]
+        return mom_node.is_child(child_node)
+
+    def cousin(self, a, b):
+        """
+        Returns a tuple of (the cousin type, degree removed) 
+
+        Keyword arguments: 
+        a -- string that is the name of node a
+        b -- string that is the name of node b
+
+        cousin type:
+          -1 if a and b are the same node.
+          -1 if either one is a direct descendant of the other
+          >=0 otherwise, it calculates the distance from 
+          each node to the common ancestor.  Then cousin type is 
+          set to the smaller of the two distances, as described 
+          in the exercises above
+
+        degrees removed:
+          >= 0
+          The absolute value of the difference between the 
+          distance from each node to their common ancestor.
+        """
+        
+        ## YOUR CODE HERE ####
+        anode = self.names_to_nodes[a]       
+        acounter = 0
+        while anode != None:
+            bnode = self.names_to_nodes[b]
+            bcounter = 0
+            while bnode != None:
+                if anode == bnode:
+                    if acounter > bcounter:
+                        t = bcounter
+                        r = acounter - bcounter
+                    else:
+                        t = acounter
+                        r = bcounter - acounter
+                    return (t-1, r)
+                bnode = bnode.get_parent()
+                bcounter += 1
+            anode = anode.get_parent()
+            acounter += 1
