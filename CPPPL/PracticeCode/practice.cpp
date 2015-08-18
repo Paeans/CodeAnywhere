001104aec2abb2ff254eb118038ee0a850a21c1b
paeans <paeans@gmail.com>
2015-08-18 12:42:18 -0400

    'Update'

--- CPPPL/PracticeCode/practice.cpp
+++ CPPPL/PracticeCode/practice.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 int main(){
     cout << "Code in Practice Cpp." << endl;
+    
     vector<int> v{1, 2, 3};
     v.push_back(10);
     cout << v.size() << endl;
+    
     return 0;
 }
