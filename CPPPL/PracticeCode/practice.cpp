10cd274d646aafa89c77f1397ab9e69eb2c7eb4a
paeans <paeans@gmail.com>
2015-08-19 03:04:04 -0400

    Update

--- CPPPL/PracticeCode/practice.cpp
+++ CPPPL/PracticeCode/practice.cpp
@@ -13,5 +13,12 @@ int main(){
     v.push_back(10);
     cout << v.size() << endl;
     
+    vector<vector<int>> mv{
+        {1, 2, 3}, 
+        {4, 5, 6},
+        {7, 8, 9}};
+    cout << mv.size() << endl;
+    cout << mv[1][1] << endl;
+    
     return 0;
 }
