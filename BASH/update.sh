d26b004aa68d831ef17fb1b4538510c6d1f939c7
paeans <paeans@gmail.com>
2015-08-19 23:06:32 -0400

    Update

---
+++ BASH/update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+[[ $1 == "" ]] && msg="Update" || msg=$1
+cd $HOME/workspace/CodeAnywhere
+remotelist="origin bucket"
+
+for remote in $remotelist
+do
+    git pull $remote master
+    [[ $? == 0 ]] || (
+        echo "ERROR between local and $remote"
+        echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        echo 
+        exit 1
+    )    
+    echo "VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV"
+    echo 
+done
+
+git add -A
+git commit -m "${msg}" && (
+echo 
+for remote in $remotelist
+do
+    git push $remote master
+    echo
+done ) || echo 
