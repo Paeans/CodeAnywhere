38b3c471ed9eeb0c72da82d15356c0c55301e9b8
paeans <paeans@gmail.com>
2015-08-19 22:23:37 -0400

    Update

--- CPPPL/practice.cpp
+++ CPPPL/practice.cpp
@@ -11,15 +11,35 @@ int main(){
     
     vector<int> v{1, 2, 3};
     v.push_back(10);
-    cout << v.size() << endl;
-    
+    //cout << v.size() << endl;
+        
     vector<vector<int>> mv{
         {1}, 
         {2, 3},
         {4, 5, 6}}; 
     mv.push_back({7, 8, 9, 0});
-    cout << mv.size() << endl;
-    cout << mv[3][2] << endl;
+    //cout << mv.size() << endl;
+    //cout << mv[3][2] << endl;
     
+    vector<string> words;
+    cout << "Input words: ";
+    for(string temp; cin >> temp; )
+        words.push_back(temp);
+    sort(words.begin(), words.end());
+    string pre = "";
+    for(string temp : words)
+        if(pre != temp){
+            cout << temp << " ";
+            pre = temp;
+        }
+    cout << endl;
     return 0;
 }
+
+
+
+
+
+
+
+
