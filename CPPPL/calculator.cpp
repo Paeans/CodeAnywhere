361dc6534196f483bd91373c63087f893c197f6b
paeans <paeans@gmail.com>
2015-08-27 10:50:19 -0400

    Update

--- CPPPL/calculator.cpp
+++ CPPPL/calculator.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-double expression();
 double primary();
 double term();
+double expression();
 
 class Token{
 public:
@@ -23,21 +23,39 @@ private:
     Token buffer;
 };
 
+Token_Stream ts;
+
 int main()
 try{
     
     //Token t{'a', 0.1};
     //cout << t.kind << " " << t.value << endl;
-    Token_Stream ts;
-    Token t;
-    do{
-        t = ts.get();
-        cout << t.kind << " " << t.value << endl;
-    }while(t.kind != 'q');
+    //Token_Stream ts;
+    //Token t;
+    //do{
+    //    t = ts.get();
+    //    cout << t.kind << " " << t.value << endl;
+    //}while(t.kind != 'q');
+    
+    double result = 0;
+    while(cin){
+        Token t = ts.get();
+        if(t.kind == 'q') break;
+        if(t.kind == ';') 
+            cout << "=" << result << endl;
+        else
+            ts.putback(t);
+        result = expression();
+    }
     
     return 0;
-}catch(...){
-    cout << "ERROR in calculator" <<endl;
+}
+catch(runtime_error& ex){
+    cout << ex.what() << endl;
+    return 1;
+}
+catch(...){
+    cout << "UNKNOWN ERROR in calculator" <<endl;
     return -1;
 }
 
@@ -72,4 +90,76 @@ void Token_Stream::putback(Token t){
         throw runtime_error("putback to full buffer");
     buffer = t;
     full = true;
+}
+
+double primary(){
+    Token t = ts.get();
+    switch(t.kind){
+        case '(':{
+            double e = expression();
+            t=ts.get();
+            if(t.kind != ')') 
+                throw runtime_error("the char ) expected");
+            return e;
+        }
+        
+        case '8':
+            return t.value;
+        
+        case ';':
+        case 'q':
+            ts.putback(t);
+            return 0;
+        
+        default:
+            throw runtime_error("primary expected");
+    }
+}
+
+double term(){
+    double left = primary();
+    Token t = ts.get();
+    
+    while(true){
+        switch(t.kind){
+            case '*':
+                left *= primary();
+                t = ts.get();
+                break;
+            
+            case '/':{
+                double p = primary();
+                if(p == 0) throw runtime_error("divide by zero");
+                left /= p;
+                t = ts.get();
+                break;
+            }
+                
+            default:
+                ts.putback(t);
+                return left;            
+        }
+    }
+}
+
+double expression(){
+    double left = term();
+    Token t = ts.get();
+    while(true){
+        switch(t.kind){
+            case '+':
+                left += term();
+                t = ts.get();
+                break;
+            
+            case '-':
+                left -= term();
+                t = ts.get();
+                break;
+            
+            default:
+                ts.putback(t);
+                return left;
+        }
+    }
 }
