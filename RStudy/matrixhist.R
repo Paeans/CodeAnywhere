40047616f1477e151b2363f6c5df796377cf26ea
paeans <paeans@gmail.com>
2015-10-13 05:07:16 +0000

    Update

--- RStudy/matrixhist.R
+++ RStudy/matrixhist.R
@@ -5,12 +5,10 @@ dat<-read.table(datafile)
 vdat <- unlist(dat)
 
 jpeg(file = "matrixhist.jpeg")
-op <- par(mfrow = c(1, 2))
+par(mfrow = c(1, 2))
 
-colors <- c("red", "yellow", "green", "violet", "orange", "blue", "pink", "cyan")
-hist(vdat, breaks = c(0.1 * -10 : 10), col = colors)
-hist(vdat, breaks = c(0.05 * -20 : 20), col = colors)
-par(op)
+hist(vdat, breaks = c(0.1 * -10 : 10), col = "green")
+hist(vdat, breaks = c(0.05 * -20 : 20), col = "red")
 
 dev.off()
 
