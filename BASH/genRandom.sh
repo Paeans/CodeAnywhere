6f909967f06bc62af3343620450e2bc58d80f136
paeans <paeans@gmail.com>
2015-10-12 20:36:30 -0400

    Update

---
+++ BASH/genRandom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## gen $2 or 100 Random between 0 ~ 1
+#  result store in file $1
+
+[[ $1 == "" ]] && file="data.txt" || file=$1
+[[ $2 == "" ]] && num=100 || num=$2
+
+for i in `seq $(( $num * $num ))`
+do
+  [[ $(( $RANDOM % 10 )) > 4 ]] && tag="+" || tag="-"
+  echo -n -e ${tag}0.$(( ${RANDOM}*999983%1000000 ))\\t >> $file
+  [[ $(( $i % $num)) == 0 ]] && echo >> $file
+done
+
