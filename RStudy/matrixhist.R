fc1d3e83efd0b7e11a64f4d17c7e04f2690dfbfa
paeans <paeans@gmail.com>
2015-10-13 04:57:22 +0000

    Update

--- RStudy/matrixhist.R
+++ RStudy/matrixhist.R
@@ -1,16 +1,17 @@
 ## Rscript saveplot.r #arg1 arg2
 
-jpeg(file = "matrixhist.jpeg")
-op <- par(mfrow = c(2, 2))
 datafile <- "../PPC/matrix.txt"
 dat<-read.table(datafile)
 vdat <- unlist(dat)
+
+jpeg(file = "matrixhist.jpeg")
+op <- par(mfrow = c(2, 2))
+par(op)
 
 colors <- c("red", "yellow", "green", "violet", "orange", "blue", "pink", "cyan")
 hist(vdat, breaks = c(0.1 * -10 : 10), col = colors)
 hist(vdat, breaks = c(0.05 * -20 : 20), col = colors)
 
-par(op)
 dev.off()
 
 q(status = 0)
