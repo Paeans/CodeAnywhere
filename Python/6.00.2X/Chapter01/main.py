7feab60383a9ca9937a1b49c42d62b7843d035f4
paeans <paeans@gmail.com>
2015-10-23 10:30:15 -0400

    Update

--- Python/6.00.2X/Chapter01/main.py
+++ Python/6.00.2X/Chapter01/main.py
@@ -45,4 +45,3 @@ lotsOfParameters3(1, c=2, b=3)              # 1 3 2 4 5
 lotsOfParameters3(1,e=5,d=4,c=3,b=2)        # 1 3 2 4 5
 lotsOfParameters3(e=5,d=4,c=3,b=2,1)        # ERROR
 lotsOfParameters3(e=5,a=1,d=4,b=2,c=3)      # 1 2 3 4 5
-
